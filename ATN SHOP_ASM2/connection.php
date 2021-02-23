7a450bd3aa5a59658d978bc14116ce318afe4235
vanminh.le <vanminh.le.754@gmail.com>
2021-02-23 17:47:50 +0700

    Update connection.php

--- ATN SHOP_ASM2/connection.php
+++ ATN SHOP_ASM2/connection.php
@@ -3,13 +3,13 @@
 $conn = "";
 
 try {
-  $servername = "localhost:3306";
-  $dbname = "loginPage";
-  $username = "root";
-  $password = "";
+  $servername = "ec2-54-74-156-137.eu-west-1.compute.amazonaws.com";
+  $dbname = "d7fkqs4e2revk2";
+  $username = "adocsvsxqmurpc";
+  $password = "c3beef8fa5925724b28a930ce5df54d58341c4afe915e1ca1bf29643f326491f";
 
   $conn = new PDO(
-    "mysql:host=$servername; dbname=loginPage",
+    "mysql:host=$servername; dbname=d7fkqs4e2revk2",
     $username,
     $password
   );
