5d90d9c90c2387335d757ed663982c73c3b1cd7b
vanminh.le <vanminh.le.754@gmail.com>
2021-02-23 18:11:19 +0700

    Update connection.php

--- ATN SHOP_ASM2/connection.php
+++ ATN SHOP_ASM2/connection.php
@@ -1,21 +1,15 @@
 <?php
 
-try {
-  $servername = "ec2-54-74-156-137.eu-west-1.compute.amazonaws.com";
-  $dbname = "d7fkqs4e2revk2";
-  $username = "adocsvsxqmurpc";
-  $password = "c3beef8fa5925724b28a930ce5df54d58341c4afe915e1ca1bf29643f326491f";
 
-  $conn = new PDO(
-    "mysql:host=$servername; dbname=$dbname",
-    $username,
-    $password
+try {
+  $dbConn = pg_connect("host=ec2-54-74-156-137.eu-west-1.compute.amazonaws.com port=5432 dbname=d7fkqs4e2revk2 user=adocsvsxqmurpc
   );
 
-  $conn->setAttribute(
+  $dbConn->setAttribute(
     PDO::ATTR_ERRMODE,
     PDO::ERRMODE_EXCEPTION
   );
 } catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
+
