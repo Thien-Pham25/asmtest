dbe77a690012d42dc620fee01732261cb4de015b
vanminh.le <vanminh.le.754@gmail.com>
2021-02-26 10:10:19 +0700

    Delete ATN SHOP_ASM2 directory

--- ATN SHOP_ASM2/bosslogin/bosslogout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['username'])) {
-  session_destroy();
-  header('Location: ./bosslogout/index.php');
-} else {
-  header('Location: ./bosslogout/index.php');
-}
